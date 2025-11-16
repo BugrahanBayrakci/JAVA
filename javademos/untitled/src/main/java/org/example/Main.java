b7e8f019cf8bb2598dea92bc407e7b22ad0bf38f
BugrahanBayrakci <youlostbtw@gmail.com>
2025-11-16 22:13:22 +0300

    Update Main.java

--- javademos/untitled/src/main/java/org/example/Main.java
+++ javademos/untitled/src/main/java/org/example/Main.java
@@ -103,10 +103,79 @@ public class Main {
         for(int i=0;i<4;i++){
             System.out.println(dizi2[i]);
         }
+        //2 Boyutlu dizi 1
+        int [] [] sayiliste=new int[3][3];
+
+
+        for (int i = 0; i < sayiliste.length; i++) {          // satır
+            for (int j = 0; j < sayiliste[i].length; j++) {   // sütun
+                System.out.print(sayiliste[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("----------------------------------");
+
+        int[][] sayilar3 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int i = 0; i < sayilar3.length; i++) {          // satır
+            for (int j = 0; j < sayilar3[i].length; j++) {   // sütun
+                System.out.print(sayilar3[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("-----------STRING İSLEMLERİ------------------");
+
+
+        String ad = "Ahmet";
+        System.out.println("Number of character: "+ad.length()); // karakter sayısı
+
+        String s = "Java";
+        System.out.println("Number of 0 character : "+s.charAt(0)); // J
+
+        String merhaba = "Merhaba";
+        System.out.println("Characters 0-3"+merhaba.substring(0, 3)); // Mer
+        System.out.println("Character 3-"+merhaba.substring(3));    // haba
+
+        System.out.println("String start with mer: "+merhaba.startsWith("Mer")); // true
+        System.out.println("String end with mer:"+merhaba.endsWith("ba"));    // true
+
+        System.out.println("index of the first letter a: "+merhaba.indexOf("a"));
+        System.out.println("index of the last letter a: "+merhaba.lastIndexOf("a"));
+
+        System.out.println("Araba".replace("a", "x"));  // Arxbx harfleri değiştirme
+
+        String cumle = "Ben Java öğreniyorum";
+// split isteninlen regexe göre kelimelere ayır.
+        for (String kelime : cumle.split(" ")){
+            System.out.println(kelime);
+
+        }
+
+        s.toUpperCase();  // MERHABA
+        s.toLowerCase();  // merhaba
+
+// Başındaki ve sonundaki boşlukları siler
+        String trimkelene = "   Merhaba Dünya   ";
+        System.out.println(trimkelene.trim());
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
